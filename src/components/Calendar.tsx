159764887aa2e58c7d2a9dd940bd1cdea71604f6
Angad Nagar <113714156+angadnagar@users.noreply.github.com>
2024-07-30 17:41:28 +0530

    Update Calendar.tsx

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -47,7 +47,7 @@ export const Calendar: React.FC<CalendarProps> = ({ selectedDate, onDateChange }
           onChange={handleMonthChange}
           className="block w-1/2 pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
         >
-          {months.map((month,index) => (
+          {months.map((month) => (
             <option key={month} value={month}>
               {month}
             </option>
